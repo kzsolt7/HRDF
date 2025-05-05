6fdb22dfaa1e418ab8e82711bbb17973962d9c3f
kzsolt7 <zsolt.kurt@gmail.com>
2025-05-05 23:41:13 +0200

    init

---
+++ src/main/java/com/example/hrdf/HrdfApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrdfApplication.class, args);
+	}
+
+}
