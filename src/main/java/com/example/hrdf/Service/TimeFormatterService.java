68069ee5d5480663dcd072d31a559fa1bcc30263
kzsolt7 <zsolt.kurt@gmail.com>
2025-05-05 23:50:10 +0200

    constants

--- src/main/java/com/example/hrdf/Service/TimeFormatterService.java
+++ src/main/java/com/example/hrdf/Service/TimeFormatterService.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TimeFormatterService {
 
+    static final int SECONDS_IN_MINUTES = 60;
+    static final int SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTES;
+    static final int SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
+    static final int SECONDS_IN_YEAR = 365 * SECONDS_IN_DAY;
+
     public String formatTime(int seconds) {
         int hours = seconds / 3600;
         int minutes = (seconds % 3600) / 60;
